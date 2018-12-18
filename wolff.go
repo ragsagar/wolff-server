74ae72802d6e0686f27fb1281ea90b1faf1df299
Rag Sagar <rag@aymcommerce.com>
2018-12-19 01:02:54 +0400

    Use viper to fetch config data.

--- wolff.go
+++ wolff.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/ragsagar/wolff/server"
 	"github.com/ragsagar/wolff/store"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	dataStore := store.NewSQLStore("wolffapp", "password", "wolffapp", "localhost:5432")
+	viper.AddConfigPath("config")
+	env := os.Getenv("WOLFF_ENV")
+	if env == "PRODUCTION" {
+		viper.SetConfigName("production")
+	} else if env == "STAGING" {
+		viper.SetConfigName("staging")
+	} else {
+		viper.SetConfigName("dev")
+	}
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatalln("Fatal error reading config file", err)
+	}
+	dataStore := store.NewSQLStore(viper.GetString("DB_NAME"), viper.GetString("DB_PASSWORD"), viper.GetString("DB_USER"), viper.GetString("DB_SERVER"))
 	srv := server.NewServer(dataStore)
-	srv.Run(":8080")
+	srv.Run(fmt.Sprintf(":%s", viper.GetString("APP_SERVER_PORT")))
 }
